cd492eb08f078773b7842189132d87c137ff7f46
Joantha21 <thanhkiethuynh21022005@gmail.com>
2025-11-09 05:15:52 -0700

    change

--- daddychill.py
+++ daddychill.py
@@ -1,69 +1,147 @@
-import numpy as np # linear algebra
-import pandas as pd 
-import matplotlib.pyplot as plt
-import tensorflow
-
-from scipy import signal
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
 import tensorflow as tf
-from tensorflow.python import keras
-from keras.utils import to_categorical
-from keras import layers, Model
-from keras import Sequential
-from keras.optimizers import SGD
-
-from keras.layers import Dense, Dropout
-from keras.layers import Embedding
-from keras.layers import LSTM
-tf.keras.backend.clear_session()
-from sklearn.metrics import ConfusionMatrixDisplay
-from sklearn import datasets, tree, linear_model, svm
-from sklearn.metrics import confusion_matrix,classification_report
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
-
-from sklearn.metrics import confusion_matrix,classification_report
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix
 
+from scipy.io import loadmat
+import os
+from functools import reduce
+from scipy import signal
+from scipy.stats import entropy
+from scipy.fft import fft, ifft
+import pandas as pd
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from tensorflow import keras as K
+import matplotlib.pyplot as plt
+import scipy
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold,cross_validate
+from keras.layers import Dense, Activation, Flatten, concatenate, Input, Dropout, LSTM, Bidirectional,BatchNormalization,PReLU,ReLU,Reshape
+# from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.metrics import classification_report
+from keras.models import Sequential, Model, load_model
+import matplotlib.pyplot as plt;
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from sklearn.decomposition import PCA
+from tensorflow import keras
+from sklearn.model_selection import cross_val_score
+from keras.layers import Conv1D,Conv2D,Add
+from keras.layers import MaxPool1D, MaxPooling2D
 import seaborn as sns
-import time
-import math
-import pickle
 
-data = pd.read_csv('eeg.csv')
-data.describe()
+import warnings
+warnings.filterwarnings('ignore')
 
-#Calling above function and splitting dataset into train and test
-pd.set_option('future.no_silent_downcasting', True)
-X,Y = Transform_data(data)
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 4)
 
-x_train.shape[1]
-len(x_train)
+df = pd.read_csv('EEG_Eye_State_Classification.csv')
+df.info()
+df.describe()
+df.isnull().sum()
 
-x_train = np.asarray(x_train); x_test = np.asarray(x_test)
-y_train = np.asarray(y_train); y_test = np.asarray(y_test)
+plt.figure(figsize = (15,15))
+cor_matrix = df.corr()
+sns.heatmap(cor_matrix,annot=True)
 
-num_classes = int(np.unique(y_train).size)
-assert y_train.shape[1] == 3
-num_classes = 3
 
-def create_model(input_dim, num_classes, model_name = "Daddy"):
-    inputs = layers.Input(shape = (input_dim,), name = "inputs")
-    x = layers.Lambda(lambda t: tf.expand_dims(t, axis = -1), name = "expand_dims")(inputs)  
-    x = layers.GRU(256, return_sequences = True, name = "gru")(x)  
-    x = layers.Flatten(name = "flatten")(x)                      
-    outputs = layers.Dense(num_classes, activation = "softmax", name = "dense")(x)
-    
-    return Model(inputs, outputs, name = model_name)
+# Plotting target distribution 
+plt.figure(figsize=(6,6))
+df['eyeDetection'].value_counts().plot.pie(explode=[0.1,0.1], autopct='%1.1f%%', shadow=True, textprops={'fontsize':16}).set_title("Target distribution")
 
-DaddyChill = create_model(x_train.shape[1], num_classes)
-DaddyChill.compile(optimizer = "adam", loss = "categorical_crossentropy", metrics = ["accuracy"])
-DaddyChill.summary()
+data = df.copy()
+y= data.pop('eyeDetection')
+x= data
 
-#Training and Evaluting model
-history = DaddyChill.fit(x_train, y_train, epochs = 10, validation_split = 0.1)
-loss, acc = DaddyChill.evaluate(x_test, y_test)
+x_new = StandardScaler().fit_transform(x)
+
+x_new = pd.DataFrame(x_new) 
+x_new.columns = x.columns
+
+x_train,x_test,y_train,y_test = train_test_split(x_new,y,test_size=0.15)
+x_train.shape, x_test.shape,y_train.shape,y_test.shape
+
+x_train = np.array(x_train).reshape(-1,14,1)
+x_test = np.array(x_test).reshape(-1,14,1)
+
+x_train.shape, x_test.shape,y_train.shape,y_test.shape
+
+inputs = tf.keras.Input(shape=(14,1))
+
+Dense1 = Dense(64, activation = 'relu',kernel_regularizer=keras.regularizers.l2())(inputs)
+
+#Dense2 = Dense(128, activation = 'relu',kernel_regularizer=keras.regularizers.l2())(Dense1)
+#Dense3 = Dense(256, activation = 'relu',kernel_regularizer=keras.regularizers.l2())(Dense2)
+
+lstm_1=  Bidirectional(LSTM(256, return_sequences = True))(Dense1)
+drop = Dropout(0.3)(lstm_1)
+lstm_3=  Bidirectional(LSTM(128, return_sequences = True))(drop)
+drop2 = Dropout(0.3)(lstm_3)
+
+flat = Flatten()(drop2)
+
+#Dense_1 = Dense(256, activation = 'relu')(flat)
+
+Dense_2 = Dense(128, activation = 'relu')(flat)
+outputs = Dense(1, activation='sigmoid')(Dense_2)
+
+model = tf.keras.Model(inputs, outputs)
+
+model.summary()
+
+tf.keras.utils.plot_model(model)
+
+def train_model(model,x_train, y_train,x_test,y_test, save_to, epoch = 2):
+
+        opt_adam = keras.optimizers.Adam(learning_rate=0.001)
+
+        es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10)
+        mc = ModelCheckpoint(save_to + '_best_model.h5', monitor='val_accuracy', mode='max', verbose=1, save_best_only=True)
+        lr_schedule = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 0.001 * np.exp(-epoch / 10.))
+        
+        model.compile(optimizer=opt_adam,
+                  loss=['binary_crossentropy'],
+                  metrics=['accuracy'])
+        
+        history = model.fit(x_train,y_train,
+                        batch_size=20,
+                        epochs=epoch,
+                        validation_data=(x_test,y_test),
+                        callbacks=[es,mc,lr_schedule])
+        
+        saved_model = load_model(save_to + '_best_model.h5')
+        
+        return model,history
+
+y_pred =model.predict(x_test)
+y_pred = np.array(y_pred >= 0.5, dtype = np.int)
+confusion_matrix(y_test, y_pred)
+
+print(classification_report(y_test, y_pred))
+
+model,history = train_model(model, x_train, y_train,x_test, y_test, save_to= './', epoch = 100) 
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+y_pred =model.predict(x_test)
+y_pred = np.array(y_pred >= 0.5, dtype = np.int)
+confusion_matrix(y_test, y_pred)
+
+print(classification_report(y_test, y_pred))
